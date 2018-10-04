0189474e3a5f659279144a72c68028508f0a84de
Oscar Evertsson <oscar.evertsson@live.com>
2018-10-04 10:54:24 +0200

    Created dashboard page for admins.

---
+++ frontend/src/presentationComponents/Dashboard.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Dashboard extends React.Component {
+  public render() {
+    return (
+      <div className="Dashboard">
+        <h1>Dashboard page!</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
