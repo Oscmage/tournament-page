3c93cb8c1feea5b0b600fadee0f24d1777d34816
Oscar Evertsson <oscar.evertsson@live.com>
2018-10-07 18:57:25 +0200

    Removed register from the first page.

--- frontend/src/presentationComponents/Home.tsx
+++ frontend/src/presentationComponents/Home.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { RegisterContainer } from "../containerComponents/Register";
-import Card from "./Card";
+// import Card from "./Card";
 import "../css/Home.css";
 
 class Home extends React.Component {
@@ -8,9 +7,6 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <h1>Home page</h1>
-        <Card>
-          <RegisterContainer />
-        </Card>
       </div>
     );
   }
